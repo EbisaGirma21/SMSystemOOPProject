506d392895ef4e669694e47fbcfcacb840ab0c72
EbisaGirma21 <100440432+EbisaGirma21@users.noreply.github.com>
2022-03-01 14:41:17 -0800

    update to user class from developers branch

---
+++ School Management System/User.java
@@ -0,0 +1,48 @@
+import java.sql.SQLException;
+
+public abstract class User {
+    String firstName;
+    String middleName;
+    String lastName;
+    String userId;
+    String gender;
+
+    User(String firstName, String middleName, String lastName, String userId, String gender) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.userId = userId;
+        this.gender = gender;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    abstract void setUserCon(String query) throws ClassNotFoundException, ServerCloneException;
+
+    abstract String userUpdate(String userId) throws ClassNotFoundException, SQLException;
+
+    abstract void userInserting() throws SQLException, ClassNotFoundException;
+
+    void deleteUser(String sql, String userID) throws ClassNotFoundException, SQLException {
+
+    }
+}
