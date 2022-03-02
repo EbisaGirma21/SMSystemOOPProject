6446e3c9f4ecdb5655e2d8bb5ab7ffd6132830ef
ElsabetAwraris <99953393+ElsabetAwraris@users.noreply.github.com>
2022-03-02 22:37:14 +0300

    subject class updation from loader branch

--- School Management System/Subject.java
+++ School Management System/Subject.java
@@ -1,3 +1,13 @@
-public class Subject {
-
+ public class Subject {
+        String subjectName;
+        String subjectCode;
+        String teacherId;
+        String gradeNumber;
+    
+        Subject(String subjectName, String subjectCode, String teacherId, String gradeNumber) {
+            this.subjectName = subjectName;
+            this.subjectCode = subjectCode;
+            this.teacherId = teacherId;
+            this.gradeNumber = gradeNumber;
+        }
 }
