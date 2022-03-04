1d7868bfd35755409bcb73ceea2d2142e248dcea
EbisaGirma21 <100440432+EbisaGirma21@users.noreply.github.com>
2022-03-04 04:07:39 -0800

    update to user and student class from the main branch

--- School Management System/Student.java
+++ School Management System/Student.java
@@ -2,23 +2,31 @@ import java.rmi.server.ServerCloneException;
 import java.sql.SQLException;
 
 public class Student extends User {
+    Student(String firstName, String middleName, String lastName, String userId, String gender) {
+        super(firstName, middleName, lastName, userId, gender);
+        // TODO Auto-generated constructor stub
+    }
+
     String age;
     String year;
     String gradeNumber;
+
     @Override
     void setUserCon(String query) throws ClassNotFoundException, ServerCloneException {
         // TODO Auto-generated method stub
-        
+
     }
+
     @Override
     String userUpdate(String userId) throws ClassNotFoundException, SQLException {
         // TODO Auto-generated method stub
         return null;
     }
+
     @Override
     void userInserting() throws SQLException, ClassNotFoundException {
         // TODO Auto-generated method stub
-        
+
     }
 
 }
