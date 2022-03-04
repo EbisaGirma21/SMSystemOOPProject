564119d4d589d177a47559b7caaba100eeadd6a7
ElsabetAwraris <99953393+ElsabetAwraris@users.noreply.github.com>
2022-03-04 22:41:04 +0300

    teacher update

--- School Management System/Teacher.java
+++ School Management System/Teacher.java
@@ -1,3 +1,30 @@
-public class Teacher {
 
-}
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Teacher extends User {
+    Scanner input = new Scanner(System.in);
+    Student student = new Student(null, null, null, null, null, null, null, null);
+    Marks marks = new Marks(null, null, 0, 0, 0, 0, 0);
+    String subject;
+
+    Teacher(String firstName, String middleName, String lastName, String userId, String gender, String subject) {
+        super(firstName, middleName, lastName, userId, gender);
+        this.subject = subject;
+    }
+
+    void setTeacher(String firstName, String middleName, String lastName, String userId, String gender,
+            String subject) {
+        super.firstName = firstName;
+        super.middleName = middleName;
+        super.lastName = lastName;
+        super.userId = userId;
+        super.gender = gender;
+        this.subject = subject;
+
+    }
