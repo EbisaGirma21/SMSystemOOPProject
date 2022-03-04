300bca96ca5701bdffed8aa1bfe9e37a43c4cfeb
EbisaGirma21 <100440432+EbisaGirma21@users.noreply.github.com>
2022-03-04 10:20:07 -0800

    teacher class update from the main branch

--- School Management System/Teacher.java
+++ School Management System/Teacher.java
@@ -1,3 +1,47 @@
-public class Teacher {
+import java.rmi.server.ServerCloneException;
+import java.sql.SQLException;
+
+public class Teacher extends User {
+    Teacher(String firstName, String middleName, String lastName, String userId, String gender, String subject) {
+        super(firstName, middleName, lastName, userId, gender);
+        this.subject = subject;
+    }
+
+    void setTeacher(String firstName, String middleName, String lastName, String userId, String gender,
+            String subject) {
+        super.firstName = firstName;
+        super.middleName = middleName;
+        super.lastName = lastName;
+        super.userId = userId;
+        super.gender = gender;
+        this.subject = subject;
+
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    @Override
+    void setUserCon(String query) throws ClassNotFoundException, ServerCloneException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    String userUpdate(String userId) throws ClassNotFoundException, SQLException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    void userInserting() throws SQLException, ClassNotFoundException {
+        // TODO Auto-generated method stub
+
+    }
 
 }
